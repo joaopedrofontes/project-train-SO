3ad938bec37195cd5d4dc45c593b70d08d933cee
João Pedro Fontes <joaopedrofonttes@gmail.com>
2024-12-10 21:26:37 -0300

    add mut1,2,3

--- trem.cpp
+++ trem.cpp
@@ -1,5 +1,13 @@
 #include "trem.h"
 #include <QtCore>
+#include <QMutex>
+
+QMutex mut0;
+QMutex mut1;
+
+QMutex mut4;
+
+QMutex mut6;
 
 //Construtor
 Trem::Trem(int ID, int x, int y){
@@ -14,33 +22,73 @@ void Trem::setVelocidade(int value)
 }
 //Função a ser executada após executar trem->START
 void Trem::run(){
+
     while(true){
         switch(ID){
         case 1:     //Trem 1
-            if (y == 150 && x <480)
+            if (y == 150 && x <480) {
+                if(x == 460) {
+                    mut0.lock();
+                }
                 x+=10;
-            else if (x == 480 && y < 270)
+            }
+            else if (x == 480 && y < 270) {
                 y+=10;
-            else if (x > 210 && y == 270)
+            }
+            else if (x > 210 && y == 270) {
+                if(x == 460) {
+                    mut0.unlock();
+                }
+                if(x == 360) {
+                    mut1.lock();
+                }
+                if(x == 220) {
+                    mut1.unlock();
+                }
                 x-=10;
-            else
+            }
+            else if (x == 210 && y > 150){
                 y-=10;
+            }
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 2: //Trem 2
-            if (y == 150 && x <750)
+            if (y == 150 && x <750) {
                 x+=10;
-            else if (x == 750 && y < 270)
+            }
+            else if (x == 750 && y < 270) {
+                if(y == 250) {
+                    mut4.lock();
+                }
                 y+=10;
-            else if (x > 480 && y == 270)
+            }
+            else if (x > 480 && y == 270) {
+                if(x == 500) {
+                    mut0.lock();
+                }
+                if(x == 600) {
+                    mut4.unlock();
+                }
                 x-=10;
-            else
-                y-=10;
+            }
+            else if (x == 480 && y > 150) {
+                if (y == 160) {
+                    mut0.unlock();
+                }
+                y -= 10;
+           }
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 3: //Trem 3
-            if (y == 270 && x <340)
+            if (y == 270 && x <340) {
+                if(x == 190) {
+                    mut1.lock();
+                }
+                if(x == 330) {
+                    mut1.unlock();
+                }
                 x+=10;
+            }
             else if (x == 340 && y < 390)
                 y+=10;
             else if (x > 70 && y == 390)
@@ -61,14 +109,23 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 5: //Trem 5
-            if (y == 270 && x <890)
+            if (y == 270 && x <890) {
+                if(x == 770) {
+                    mut4.unlock();
+                }
                 x+=10;
+            }
             else if (x == 890 && y < 390)
                 y+=10;
-            else if (x > 620 && y == 390)
+            else if (x > 620 && y == 390) {
                 x-=10;
-            else
+            }
+            else if (x == 620 && y > 270) {
+                if(y == 290) {
+                    mut4.lock();
+                }
                 y-=10;
+            }
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         default:
